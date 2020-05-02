9e53fadfce06c61c11fcb9241c595660721f2472
nikshanak <nikshanak@gmail.com>
2020-05-02 20:05:54 +1000

    init first_flutter_app project, other files

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       theme: ThemeData(
-        primaryColor: Colors.orangeAccent,
+        primaryColor: Colors.lightGreenAccent,
       ),
       home: RandomWords(),
     );
